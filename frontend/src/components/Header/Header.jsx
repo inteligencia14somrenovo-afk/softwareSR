873cfc24acee9b6c74077feb85a3492ab1775c7a
Som renovo <you@example.com>
2026-08-01 11:49:06 -0400

    Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,21 +1,58 @@
 import React from 'react'
-
+import { MdDarkMode, MdNotifications } from 'react-icons/md';
 import "./Header.css";
 
 const Header = () => {
+
+  const horaAtual = new Date().getHours();
+
+    let saudacao;
+
+    if (horaAtual < 12) {
+      saudacao = " Olá Bom dia";
+    } else if (horaAtual < 18) {
+      saudacao = "Olá Boa tarde";
+    } else {
+      saudacao = "Olá Boa noite";
+    }
+
+    const dataAtual = new Date().toLocaleDateString("pt-BR", {
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+    });
+
+    const usuario = "user";
+
   return (
     <header className="header">
 
       <div className="header-left">
         <p className="page-title">Dashboard</p>
 
-        <h1>Bom dia/tarde/noite(vai depender da hr real), (user) 👋</h1>
+        <h1>{saudacao}, {usuario} !</h1>
 
-        <time>** de ** de 2026</time>
+        <time>{dataAtual}</time>
       </div>
 
       <div className="header-right">
-        (🌙/sol) - (notificação)
+       
+       <button
+        className="header-icon
+        theme-button">
+          <MdDarkMode />
+       </button>
+
+          <div className="divider"></div>
+
+       <button 
+       className="header-icon
+       notification">
+          <MdNotifications />
+          <span></span>
+       </button>
+
+
       </div>
 
     </header>
