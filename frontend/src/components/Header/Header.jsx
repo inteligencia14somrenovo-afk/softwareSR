a6a507484631db6d123393d93e773db23ab8d8d1
Som renovo <you@example.com>
2026-08-01 09:10:24 -0400

    Sidebar finalizado

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -9,13 +9,13 @@ const Header = () => {
       <div className="header-left">
         <p className="page-title">Dashboard</p>
 
-        <h1>Bom dia, (user) 👋</h1>
+        <h1>Bom dia/tarde/noite(vai depender da hr real), (user) 👋</h1>
 
         <time>** de ** de 2026</time>
       </div>
 
       <div className="header-right">
-        (🌙/sol) - (notificação) - (perfil)
+        (🌙/sol) - (notificação)
       </div>
 
     </header>
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { FaHome } from "@react-icons/all-files/fa/FaHome";
 import { FaUserGraduate } from "@react-icons/all-files/fa/FaUserGraduate";
-import { FaChalkboardTeacher } from "@react-icons/all-files/fa/FaChalkboardTeacher";
 import { FaClipboardCheck } from "@react-icons/all-files/fa/FaClipboardCheck";
 import { FaBook } from "@react-icons/all-files/fa/FaBook";
 import { FaChartBar } from "@react-icons/all-files/fa/FaChartBar";
@@ -8,46 +7,88 @@ import { FaCog } from "@react-icons/all-files/fa/FaCog";
 
 import "./Sidebar.css";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { MdMenu, MdMenuOpen } from "react-icons/md";
+
 
 const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-   <div className="sidebar">
+    <aside className={`sidebar ${collapsed ? "collapsed" : ""}`}>
 
-    <img src={logo} alt="Som Renovo" className="logo" />
-    <h2>Manager</h2>
+      <div className="logo-menu">
+       {!collapsed && ( 
+        <img src={logo} alt="Som Renovo" className="logo" />
+                   )}
+        
+        {collapsed ? (
+          <MdMenu
+          className="menu-icon-2"
+          onClick={() =>
+            setCollapsed(false)}
+            />
+          ) : (
+        <MdMenuOpen 
+          className="menu-icon"
+          onClick={() => setCollapsed(true)}
+        />
+          )}
+      </div>
 
-     <nav>
-    <a href="#" className="active">
-        <FaHome />
-        <span>Dashboard</span>
-    </a>
-    <a href="#">
-        <FaUserGraduate />
-        <span>Alunos</span>
-    </a>
-    {/*<a href="#">
-        <FaChalkboardTeacher />
-        <span>Professores</span>
-    </a>*/}
-    <a href="#">
-        <FaClipboardCheck />
-        <span>Presença</span>
-    </a>
-    <a href="#">
-        <FaBook />
-        <span>Planos de aula</span>    
-    </a> 
-    <a href="#">
-        <FaChartBar />
-        <span>Relatorio</span>
-    </a>
-    <a href="#">
-        <FaCog />
-        <span>Configuração</span>
-    </a>
-     </nav>
-   </div>
+      <nav>
+        <a href="#" className="active">
+          <FaHome />
+                    {!collapsed &&
+          <span>Dashboard</span> }
+        </a>
+
+        <a href="#">
+          <FaUserGraduate />
+           {!collapsed &&
+          <span>Alunos</span> }
+        </a>
+
+        <a href="#">
+          <FaClipboardCheck />
+           {!collapsed &&
+          <span>Presença</span> }
+        </a>
+
+        <a href="#">
+          <FaBook />
+           {!collapsed &&
+          <span>Planos de aula</span> }
+        </a>
+
+        <a href="#">
+          <FaChartBar />
+           {!collapsed &&
+          <span>Relatório</span> }
+        </a>
+
+        <a href="#">
+          <FaCog />
+           {!collapsed &&
+          <span>Configuração</span> }
+        </a>
+      </nav>
+
+        <div className="profile">
+          <div
+          className="profile-image">
+
+          </div>
+            U
+          <div
+          className="profile-info">
+            <h4>nome de usuario</h4>
+            <p>email real de user</p>
+          </div>
+        </div>
+
+    </aside>
   )
 }
 
-export default Sidebar
+export default Sidebar;
